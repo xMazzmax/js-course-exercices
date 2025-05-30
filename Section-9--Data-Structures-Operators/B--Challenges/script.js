db0ca70ce85a64351483fa71bfb1532c08a7cab2
Maximiliano Ely Mazzola <max1_max@hotmail.com>
2025-05-30 07:37:04 +0200

    add completed section 9 challenge #2

--- Section-9--Data-Structures-Operators/B--Challenges/script.js
+++ Section-9--Data-Structures-Operators/B--Challenges/script.js
@@ -1,11 +1,4 @@
 "use strict";
-//#startregion
-/////////////////
-// Challenge #1
-/////////////////
-
-// We're building a football betting app.
-// Suppose we get data from a web service about a certain game. In this challenge we're gonna work with that data.
 
 const game = {
   team1: "Bayern Munich",
@@ -47,6 +40,13 @@ const game = {
     team2: 6.5,
   },
 };
+
+//////////////////////////////////
+//#startregion Challenge #1
+//////////////////////////////////
+
+// We're building a football betting app.
+// Suppose we get data from a web service about a certain game. In this challenge we're gonna work with that data.
 
 // Tasks:
 
@@ -103,4 +103,66 @@ const game = {
 //   console.log(`${game.team1} is more likely to win`);
 // game.odds.team1 > game.odds.team2 &&
 //   console.log(`${game.team2} is more likely to win`);
+//#endregion
+
+//////////////////////////////////
+//#startregion Challenge #2
+//////////////////////////////////
+
+// Let's continue with our football betting app! Keep using the 'game' variable from before.
+
+// Tasks:
+
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+
+// for (const [index, player] of Object.values(game.scored).entries())
+//   console.log(`Goal ${index + 1}: ${player}`);
+
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+
+// let startingValue = 0;
+
+// console.log(
+//   Number(
+//     (
+//       Object.values(game.odds).reduce((accumulatedValue, currentValue) => accumulatedValue + currentValue, startingValue)
+//       / Object.values(game.odds).length
+//     ).toFixed(2)
+//   )
+// );
+
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+
+//   Odds of victory Bayern Munich: 1.33
+//   Odds of draw: 3.25
+//   Odds of victory Borrussia Dortmund: 6.5
+
+// for (const [savedLabel, odd] of Object.entries(game.odds)) {
+//   let printedLabel = game[savedLabel] ? `victory ${game[savedLabel]}` : "draw";
+
+//   console.log(`Odds of ${printedLabel}: ${odd}`);
+// }
+
+// Get the team names directly from the game object, don't hardcode them (except for "draw"). Hint: Note how the odds and the game objects have the same property names
+
+// 4. Bonus: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+
+// {
+//   Gnarby: 1,
+//   Hummels: 1,
+//   Lewandowski: 2
+// }
+
+// let scorers = { Lewandowski: 0 };
+
+// for (const player of game.scored) {
+//   Object.hasOwn(scorers, player) ? scorers[player]++ : (scorers[player] = 1);
+
+//   // sample solution
+//   // scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+// }
+
+// console.log(scorers);
+
+// scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
 //#endregion

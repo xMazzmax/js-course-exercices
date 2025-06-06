b513bd69e71ebe09ea72528f5eb39c828739fcfc
Maximiliano Ely Mazzola <max1_max@hotmail.com>
2025-06-06 06:56:10 +0200

    add Maps iteration code to "follow along"

--- Section-9--Data-Structures-Operators/A--Follow-along/script.js
+++ Section-9--Data-Structures-Operators/A--Follow-along/script.js
@@ -123,26 +123,56 @@
 
 // // When a function receives an object with many properties as an argument
 // // This makes it easier to work with the properties of the object
-const order = {
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
+// const order = {
+//   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+//   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-  confirmation: function ({ starterIndex, mainIndex, deliveryTime, address }) {
-    console.log(
-      `${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${deliveryTime}`
-    );
-  },
-};
+//   confirmation: function ({ starterIndex, mainIndex, deliveryTime, address }) {
+//     console.log(
+//       `${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${deliveryTime}`
+//     );
+//   },
+// };
 
-let orderDataFromAPI = {
-  deliveryTime: "20:00",
-  address: "Zürichstrasse 1",
-  starterIndex: 1,
-  mainIndex: 2,
-};
+// let orderDataFromAPI = {
+//   deliveryTime: "20:00",
+//   address: "Zürichstrasse 1",
+//   starterIndex: 1,
+//   mainIndex: 2,
+// };
 
-order.confirmation(orderDataFromAPI);
+// order.confirmation(orderDataFromAPI);
 
 //////////////////////////////////////
-//
+// Maps iteration
 //////////////////////////////////////
+
+// const question = new Map([
+//   ["question", "What is the best programming language in the world?"],
+//   [1, "C"],
+//   [2, "Java"],
+//   [3, "JavaScript"],
+//   ["correct", 3],
+//   [true, "Correct 🎉"],
+//   [false, "Try again!"],
+// ]);
+
+// // Convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// const arrFromMap = [...question.keys()];
+// for(const [key, value] of question)
+//   objectFromMap.set
+// console.log(arrFromMap);
+
+// console.log(objectFromMap);
+
+// // Quiz app
+// console.log(question.get("question"));
+// for (const [key, value] of question) {
+//   if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+// }
+// const answer = Number(prompt("Your answer"));
+// console.log(question.get(question.get("correct") === answer));

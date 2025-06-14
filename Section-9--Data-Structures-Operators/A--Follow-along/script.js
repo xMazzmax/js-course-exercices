72b9c0e4542831e6fcc2bee6cc12057c9c47ad5a
Maximiliano Ely Mazzola <max1_max@hotmail.com>
2025-06-14 19:00:59 +0200

    Add final code to section 9 follow along

--- Section-9--Data-Structures-Operators/A--Follow-along/script.js
+++ Section-9--Data-Structures-Operators/A--Follow-along/script.js
@@ -176,3 +176,151 @@
 // }
 // const answer = Number(prompt("Your answer"));
 // console.log(question.get(question.get("correct") === answer));
+
+// //////////////////////////////////////
+// // 128. Working With Strings - Part 1
+// //////////////////////////////////////
+
+// const quote = "Follow the plan!";
+
+// // First string index
+// console.log(quote[0]); // F
+
+// // Second string index
+// console.log(quote[1]); // o
+
+// // Type in the string directly and use it like a variable of type string
+// console.log("Asdf"[1]); // s
+// console.log("Asdf".length); // 4
+
+// // Get the index of the first correspondant character found inside the string
+// console.log(quote.indexOf("l")); // 2
+
+// // Get the index of the last correspondant character found inside the string
+// console.log(quote.lastIndexOf("l")); // 12
+
+// // Get the starting index of the first correspondant string (multiple characters) found inside the string
+// console.log(quote.indexOf("plan")); // 11
+
+// // Get the starting index of the last correspondant string (multiple characters) found inside the string
+// console.log(quote.lastIndexOf("plan")); // 12
+
+// // Get a string starting from a specific index
+// console.log(quote.slice(7)); // the plan!
+
+// // Get a string with the starting and ending index. The ending index is doesn't get returned. Length of the returned string is ending - starting index
+// console.log(quote.slice(7, 10)); // the
+
+// // Get a string with the starting and ending string by using the index of a specific string. The ending string doesn't get returned.
+// console.log(quote.slice(0, quote.indexOf(" "))); // Follow
+
+// // Get a string with the starting and ending string by using the last index of a specific string + 1. The ending string doesn't get returned.
+// console.log(quote.slice(quote.lastIndexOf(" ") + 1)); // Follow
+
+// // Get a string with the starting index and an ending index by using a negative index. The negative index is the index number starting from the end.
+// console.log(quote.slice(0, -2)); // Follow the pla
+
+// //////////////////////////////////////
+// // 129. Working With Strings - Part 2
+// //////////////////////////////////////
+// const quote = "Follow The Plan!";
+
+// console.log(quote.toLowerCase()); // follow the plan!
+// console.log(quote.toUpperCase()); // FOLLOW THE PLAN!
+
+// // Fix string formatting
+// const passengerName = "mAxI";
+// const passengerNameLowerCase = passengerName.toLocaleLowerCase();
+// const passengerNameFormatted =
+//   passengerNameLowerCase[0].toUpperCase() + passengerNameLowerCase.slice(1);
+
+// console.log(passengerNameFormatted); // Maxi
+
+// // Fix email formatting
+// const emailWrongFormat = " MyMail@mail.com  \n";
+// const emailRightFormat = "mymail@mail.com";
+// const emailFormatted = emailWrongFormat.toLowerCase().trim();
+
+// console.log(emailRightFormat === emailFormatted); // true
+
+// // Replace string
+// const currencyDollar = "$20,00";
+// const currencyEuro = currencyDollar.replace("$", "€").replace(",", ".");
+
+// console.log(currencyEuro); // €20.00
+
+// // Replace all matches of a substring in a string
+// const quote2 = "Alone, alone, all, all alone.";
+// const quote2Replaced = quote2.toLowerCase().replaceAll("alone", "together"); // together, together, all, all together.
+
+// console.log(quote2Replaced);
+
+// // Replace all matches of a substring in a string with RegEx
+// const quote2Replaced2 = quote2.toLowerCase().replace(/alone/g, "together"); // together, together, all, all together.
+
+// console.log(quote2Replaced2);
+
+// // Check if a string contains a substring
+// const quoteBusiness = "The Key to success is action.";
+// console.log(quoteBusiness.toLowerCase().includes("key")); // true
+
+// // Check if a string contains a substring with a function
+// const checkBaggage = function (baggageContent) {
+//   const baggageContentLowerCase = baggageContent.toLowerCase();
+
+//   if (
+//     baggageContentLowerCase.includes("gun") ||
+//     baggageContentLowerCase.includes("knife")
+//   )
+//     console.log("You are NOT allowed on board!");
+//   else {
+//     console.log("Welcome aboard!");
+//   }
+// };
+
+// checkBaggage("I have a laptop, some Food and a pocket Knife"); // You are NOT allowed on board!
+// checkBaggage("Socks and camera"); // Welcome aboard!
+// checkBaggage("Got some snacks and a gun for protection"); // You are NOT allowed on board!
+
+// //////////////////////////////////////
+// // 129. Working With Strings - Part 3
+// //////////////////////////////////////
+
+// // Split a string into substrings using a specific separator and return them as an array
+// console.log(
+//   "Love+is+composed+of+a+single+soul+inhabiting+two+bodies".split("+")
+// ); // Array [ "Love", "is", "composed", "of", "a", "single", "soul", "inhabiting", "two", "bodies" ]
+
+// // Combine split with destructuring to easily create variables out of substrings
+// const [firstName, lastName] = "Maximiliano Ely-Mazzola".split(" ");
+
+// console.log(firstName);
+// console.log(lastName);
+
+// // Add strings together with a separator using the join method
+// console.log(["Mr.", firstName, lastName.toUpperCase()].join(" "));
+
+// // Repeat a string
+// console.log("*".repeat(10)); // **********
+
+// // Add padding at the start or end of a string for a max length (including length of the string)
+// function AddPadding(item, price) {
+//   console.log(item.padEnd(20, "_") + "CHF" + price.padStart(10, "_"));
+// }
+
+// // Banana______________CHF______0.99
+// AddPadding("Banana", "0.99");
+// // Gaming Laptop_______CHF__2'499.99
+// AddPadding("Gaming Laptop", "2'499.99");
+// // Luxury Watch________CHF_89'999.99
+// AddPadding("Luxury Watch", "89'999.99");
+
+// // Mutate a specific character index of all substrings
+// const names = "maximiliano ely mazzola".split(" ");
+// let capitalizedNames = [];
+
+// for (const n of names) {
+//   capitalizedNames.push(n.replace(n[0], n[0].toUpperCase()));
+// }
+
+// console.log(capitalizedNames.join(" ")); // Maximiliano Ely Mazzola

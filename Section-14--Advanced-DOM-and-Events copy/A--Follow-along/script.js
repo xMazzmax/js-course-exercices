8873fe37fc7b12e126750c7a928e0441ae8ae85b
Maximiliano Ely Mazzola <max1_max@hotmail.com>
2026-01-03 09:38:22 +0100

    add getters and setters

--- Section-14--Advanced-DOM-and-Events copy/A--Follow-along/script.js
+++ Section-14--Advanced-DOM-and-Events copy/A--Follow-along/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//#region 221.-224. Prototypal OOP basics
 // // in JS, constructor functions are written in PascalCase by convention
 // const Person = function (name, birthYear) {
 //   (this.name = name), (this.birthYear = birthYear);
@@ -41,46 +41,106 @@
 // // The [[Prototype]] of the end of a prototype chain (Object.prototype.[[Prototype]]) is null. If a property can't be resolved along the chain, `undefined` is returned
 // max.bla; // undefined
 // // console.log(max.bla()); // TypeError
+//#endregion
 
 //#region 226. ES6 Classes
-// Class declaration
+// // Class declaration
+// class Person {
+//   constructor(name, birthYear) {
+//     this.name = name;
+//     this.birthYear = birthYear;
+//   }
+
+//   calculateAge() {
+//     console.log(new Date().getFullYear() - this.birthYear);
+//   }
+// }
+
+// const isAdmin = true;
+
+// // Class expression
+// // useful for conditionals
+// const User = isAdmin
+//   ? class Guest {
+//       constructor(name) {
+//         this.name = name;
+//       }
+
+//       read() {}
+//     }
+//   : class Admin {
+//       constructor(name) {
+//         this.name = name;
+//       }
+
+//       read() {}
+//       write() {}
+//       delete() {}
+//     };
+
+// const max = new Person("Max", 1990);
+// max.calculateAge();
+
+// // Classes are NOT hoisted (unlike function declarations)
+// // Classes are always executed in strict mode
+// // Classes are first class citizens (can be passed into and returned from functions)
+//#endregion
+
+//#region 227. Setters and Getters
+// Every object in JS can have ´getter´ and ´setter´ properties. These properties are called "accessor properties", while regular properties are called "data properties". Setters and getters can be implemented independently of one another (e.g. defining just 1 of them).
+
+const account = {
+  owner: "Max",
+  movements: [100, -50, 200, 500],
+
+  // getter that returns the last value of the array
+  get latestMovement() {
+    return this.movements[this.movements.length - 1];
+  },
+
+  // setter that adds a value to the end of the array
+  // every setter needs to have exactly 1 parameter to be set
+  set latestMovement(movement) {
+    this.movements.push(movement);
+  },
+};
+
+// even though they contain methods, getters and setters are used like regular properties
+account.latestMovement; // gets 500
+account.latestMovement = -200; // sets -200
+account.latestMovement; // gets -200
+
+account.movements; // [100, -50, 200, 500, -200]
+
+// Since classes are fundamentally objects, they can also create getters and setters
 class Person {
-  constructor(name, birthYear) {
+  constructor(name, birthYear, sex) {
     this.name = name;
     this.birthYear = birthYear;
+    this.sex = sex;
   }
 
   calculateAge() {
     console.log(new Date().getFullYear() - this.birthYear);
   }
+
+  get sex() {
+    return this._sex;
+  }
+
+  // setters are great for validation
+  set sex(sex) {
+    sex === "m" || sex === "f"
+      ? (this._sex = sex)
+      : console.error(
+          `Error: The sex property was not set because the value "${sex}" is invalid. It must be either "m" or "f".`
+        );
+  }
 }
 
-const isAdmin = true;
-
-// Class expression
-// useful for conditionals
-const User = isAdmin
-  ? class Guest {
-      constructor(name) {
-        this.name = name;
-      }
-
-      read() {}
-    }
-  : class Admin {
-      constructor(name) {
-        this.name = name;
-      }
-
-      read() {}
-      write() {}
-      delete() {}
-    };
-
-const max = new Person("Max", 1990);
-max.calculateAge();
-
-// Classes are NOT hoisted (unlike function declarations)
-// Classes are always executed in strict mode
-// Classes are first class citizens (can be passed into and returned from functions)
+const jane = new Person("Jane", 1985, "female");
+jane.sex; // undefined
+jane.sex = "f";
+jane.sex; // f
+jane; // { name: "Jane", birthYear: 1985, _sex: "f" }
 //#endregion

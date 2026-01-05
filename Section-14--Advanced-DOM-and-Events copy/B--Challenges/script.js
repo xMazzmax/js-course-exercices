27dee5256bc215d515f06cea77097c7f10e7e4f7
Maximiliano Ely Mazzola <max1_max@hotmail.com>
2026-01-05 06:36:29 +0100

    add completed section 14 challenge #2

--- Section-14--Advanced-DOM-and-Events copy/B--Challenges/script.js
+++ Section-14--Advanced-DOM-and-Events copy/B--Challenges/script.js
@@ -3,41 +3,88 @@
 //////////////////////////////////////////////////////////////////////
 //#region Coding Challenge #1
 //////////////////////////////////////////////////////////////////////
-// 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 'speed' property. The 'speed' property is the current speed of the car in km/h.
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// // 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 'speed' property. The 'speed' property is the current speed of the car in km/h.
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-// 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console.
-Car.prototype.accelerate = function () {
-  this.speed = parseInt(this.speed) + 10 + " km/h";
-  console.log(this.speed);
-};
+// // 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console.
+// Car.prototype.accelerate = function () {
+//   this.speed = parseInt(this.speed) + 10 + " km/h";
+//   console.log(this.speed);
+// };
 
-// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console.
-Car.prototype.brake = function () {
-  this.speed = parseInt(this.speed) - 5 + " km/h";
-  console.log(this.speed);
-};
+// // 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console.
+// Car.prototype.brake = function () {
+//   this.speed = parseInt(this.speed) - 5 + " km/h";
+//   console.log(this.speed);
+// };
 
-// 4. Create 2 'Car' objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
-const bmw = new Car("BMW", "120 km/h");
-const mercedes = new Car("BMW", "95 km/h");
+// // 4. Create 2 'Car' objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+// const bmw = new Car("BMW", "120 km/h");
+// const mercedes = new Car("BMW", "95 km/h");
 
-console.log("----- BMW -----");
-console.log(bmw);
-bmw.accelerate();
-bmw.accelerate();
-bmw.brake();
+// // Test data:
+// // - Data car 1: 'BMW' going at 120 km/h
+// // - Data car 2: 'Mercedes' going at 95 km/h
 
-console.log("----- Mercedes -----");
-console.log(mercedes);
-mercedes.accelerate();
-mercedes.accelerate();
-mercedes.brake();
+// console.log("----- BMW -----");
+// console.log(bmw);
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.brake();
 
-// Test data:
-// - Data car 1: 'BMW' going at 120 km/h
-// - Data car 2: 'Mercedes' going at 95 km/h
+// console.log("----- Mercedes -----");
+// console.log(mercedes);
+// mercedes.accelerate();
+// mercedes.accelerate();
+// mercedes.brake();
+//#endregion
+
+//////////////////////////////////////////////////////////////////////
+//#region Coding Challenge #2
+//////////////////////////////////////////////////////////////////////
+// // 1. Use a class to implement a 'Car'. A car has a 'make' and a 'speed' property. The 'speed' property is the current speed of the car in km/h
+// // 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console
+// // 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console
+// // 4. Create 2 'Car' objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them
+// class Car {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+
+//   get speedUS() {
+//     console.log(parseInt(this.speed) / 1.6 + " mph");
+//   }
+
+//   set speedUS(_speed) {
+//     this.speed = parseInt(_speed) * 1.6 + " km/h";
+//   }
+
+//   accelerate() {
+//     this.speed = parseInt(this.speed) + 10 + " km/h";
+//     console.log(this.speed);
+//   }
+
+//   brake() {
+//     this.speed = parseInt(this.speed) - 5 + " km/h";
+//     console.log(this.speed);
+//   }
+// }
+
+// const ford = new Car("Ford", "120 km/h");
+
+// // Test data:
+// // - Data car 1: 'Ford' going at 120 km/h
+
+// console.log("----- Ford -----");
+// console.log(ford);
+// ford.accelerate();
+// ford.accelerate();
+// ford.brake();
+// console.log(ford.speedUS);
+// ford.speedUS = "125 mph";
+// console.log(ford.speed);
 //#endregion

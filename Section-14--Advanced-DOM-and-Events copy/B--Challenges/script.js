1570d63a10d69d382b70ce53e94a42cabe0a3bd6
Maximiliano Ely Mazzola <max1_max@hotmail.com>
2026-01-08 07:01:20 +0100

    add completed section 14 coding challenge #3

--- Section-14--Advanced-DOM-and-Events copy/B--Challenges/script.js
+++ Section-14--Advanced-DOM-and-Events copy/B--Challenges/script.js
@@ -88,3 +88,55 @@
 // ford.speedUS = "125 mph";
 // console.log(ford.speed);
 //#endregion
+
+//////////////////////////////////////////////////////////////////////
+//#region Coding Challenge #3
+//////////////////////////////////////////////////////////////////////
+// // 1. Use a constructor function to implement an Electric Car (called 'EV') as a child "class" of 'Car'. Besides a make and current speed, the 'EV' also has the current battery charge in % ('charge' property)
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function () {
+//   this.speed = parseInt(this.speed) + 10 + " km/h";
+//   console.log(this.speed);
+// };
+
+// Car.prototype.brake = function () {
+//   this.speed = parseInt(this.speed) - 5 + " km/h";
+//   console.log(this.speed);
+// };
+
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
+
+// EV.prototype = Object.create(Car.prototype);
+
+// // 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo'
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   if (parseFloat(this.charge) < chargeTo) this.charge = `${chargeTo}%`;
+// };
+
+// // 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%'
+// EV.prototype.accelerate = function () {
+//   this.speed = `${parseInt(this.speed) + 20} km/h`;
+//   this.charge = `${parseFloat(this.charge) - 1}%`;
+
+//   console.log(
+//     `${this.make} going at ${this.speed}, with a charge of ${this.charge}`
+//   );
+// };
+
+// // 4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! Hint: Review the definiton of polymorphism
+// // Test data:
+// // -  Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
+// const ev = new EV("Tesla", "120 km/h", "23%");
+// console.log(ev); // { make: "Tesla", speed: "120 km/h", charge: "23%" }
+// ev.accelerate(); // Tesla going at 140 km/h, with a charge of 22%
+// ev.brake(); // 135 km/h
+// ev.chargeBattery(80);
+// console.log(ev); // { make: "Tesla", speed: "135 km/h", charge: "80%" }
+//#endregion

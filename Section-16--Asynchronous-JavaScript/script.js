1d86e49a232eff1ad21670210ab093077524ba1b
Maximiliano Ely Mazzola <max1_max@hotmail.com>
2026-02-26 07:17:56 +0100

    add "callback hell" (nested callbacks) example

--- Section-16--Asynchronous-JavaScript/script.js
+++ Section-16--Asynchronous-JavaScript/script.js
@@ -11,40 +11,59 @@ const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
+const renderCountry = function (country, neighbor = false) {
+  const countriesContainer = document.querySelector(".countries");
+  const countryTemplate = document.getElementById("country-template");
+
+  const countryTemplateClone = countryTemplate.content.cloneNode(true);
+
+  const countryElement = countryTemplateClone.querySelector(".country");
+  const imageElement = countryTemplateClone.querySelector(".country__img");
+  const nameElement = countryTemplateClone.querySelector(".country__name");
+  const regionElement = countryTemplateClone.querySelector(".country__region");
+  const populationElement =
+    countryTemplateClone.querySelector("[data-population]");
+  const languageElement = countryTemplateClone.querySelector("[data-language]");
+  const currencyElement = countryTemplateClone.querySelector("[data-currency]");
+
+  if (neighbor) countryElement.classList.add("neighbour");
+  imageElement.src = country.flag;
+  nameElement.textContent = country.name;
+  regionElement.textContent = country.region;
+  populationElement.textContent = (country.population / 1000000).toFixed(1);
+  languageElement.textContent = country.languages[0].name;
+  currencyElement.textContent = country.currencies[0].name;
+
+  countriesContainer.append(countryTemplateClone);
+
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndCountryNeighborData = function (country) {
   const request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.com/v2/name/${country}`);
   request.send();
   request.addEventListener("load", function () {
-    const [response] = JSON.parse(this.response);
-    const countriesContainer = document.querySelector(".countries");
-    const countryTemplate = document.getElementById("country-template");
+    const [country] = JSON.parse(request.response);
+    renderCountry(country);
 
-    const countryTemplateClone = countryTemplate.content.cloneNode(true);
+    const countryNeighborCode = country.borders?.[0];
 
-    const imageElement = countryTemplateClone.querySelector(".country__img");
-    const nameElement = countryTemplateClone.querySelector(".country__name");
-    const regionElement =
-      countryTemplateClone.querySelector(".country__region");
-    const populationElement =
-      countryTemplateClone.querySelector("[data-population]");
-    const languageElement =
-      countryTemplateClone.querySelector("[data-language]");
-    const currencyElement =
-      countryTemplateClone.querySelector("[data-currency]");
+    if (!countryNeighborCode) return;
 
-    imageElement.src = response.flag;
-    nameElement.textContent = response.name;
-    regionElement.textContent = response.region;
-    populationElement.textContent = (response.population / 1000000).toFixed(1);
-    languageElement.textContent = response.languages[0].name;
-    currencyElement.textContent = response.currencies[0].name;
-
-    countriesContainer.append(countryTemplateClone);
-
-    countriesContainer.style.opacity = 1;
+    const countryNeighborRequest = new XMLHttpRequest();
+    countryNeighborRequest.open(
+      "GET",
+      `https://restcountries.com/v2/alpha/${countryNeighborCode}`,
+    );
+    countryNeighborRequest.send();
+    countryNeighborRequest.addEventListener("load", function () {
+      const countryNeighbor = JSON.parse(countryNeighborRequest.response);
+      console.log(countryNeighbor);
+      renderCountry(countryNeighbor, true);
+    });
   });
 };
 
-getCountryData("portugal");
-getCountryData("usa");
+getCountryAndCountryNeighborData("Germany");
+// getCountryAndCountryNeighborData("usa");

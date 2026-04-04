0fe9161038810c0e37f24417a163c950261d070b
Maximiliano Ely Mazzola <max1_max@hotmail.com>
2026-04-04 22:32:53 +0200

    add first AJAX call using XMLHttpRequest

--- Section-16--Asynchronous-JavaScript/A--Follow-along/script.js
+++ Section-16--Asynchronous-JavaScript/A--Follow-along/script.js
@@ -2,3 +2,46 @@
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
+//////////////////////////////////////////////////////////////////////
+// #region 260. Our First AJAX Call: XMLHttpRequest
+/////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  // request type + API URL
+  request.open("GET", `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  request.addEventListener("load", function () {
+    // AJAX requests return the response in JSON format
+    const [data] = JSON.parse(this.responseText);
+    // => { name: "Portugal", alpha2Code: "PT", alpha3Code: "PRT", ... }
+
+    const countryTemplate = document.getElementById("country-template");
+
+    const countryTemplateClone = countryTemplate.content.cloneNode(true);
+
+    const imageElement = countryTemplateClone.querySelector(".country__img");
+    const nameElement = countryTemplateClone.querySelector(".country__name");
+    const regionElement =
+      countryTemplateClone.querySelector(".country__region");
+    const populationElement =
+      countryTemplateClone.querySelector("[data-population]");
+    const languageElement =
+      countryTemplateClone.querySelector("[data-language]");
+    const currencyElement =
+      countryTemplateClone.querySelector("[data-currency]");
+
+    imageElement.src = data.flag;
+    nameElement.textContent = data.name;
+    regionElement.textContent = data.region;
+    populationElement.textContent = (data.population / 1000000).toFixed(1);
+    languageElement.textContent = data.languages[0].name;
+    currencyElement.textContent = data.currencies[0].name;
+
+    countriesContainer.append(countryTemplateClone);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData("Portugal");
+// #endregion
+//////////////////////////////////////////////////////////////////////

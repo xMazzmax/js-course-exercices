05eaecf54559a7a6f51ea04ced9175a8be19b37a
Maximiliano Ely Mazzola <max1_max@hotmail.com>
2026-04-04 22:59:24 +0200

    add first AJAX call using Promises

--- Section-16--Asynchronous-JavaScript/A--Follow-along/script.js
+++ Section-16--Asynchronous-JavaScript/A--Follow-along/script.js
@@ -49,6 +49,83 @@ const countriesContainer = document.querySelector(".countries");
 //////////////////////////////////////////////////////////////////////
 // #region 262. Welcome to Callback Hell
 ///////////////////////////////////
+// const renderCountry = function (
+//   country,
+//   { isNeighboringCountry = false } = {},
+// ) {
+//   const countryTemplate = document.getElementById("country-template");
+
+//   const countryTemplateClone = countryTemplate.content.cloneNode(true);
+
+//   const countryElement = countryTemplateClone.querySelector(".country");
+//   const imageElement = countryTemplateClone.querySelector(".country__img");
+//   const nameElement = countryTemplateClone.querySelector(".country__name");
+//   const regionElement = countryTemplateClone.querySelector(".country__region");
+//   const populationElement =
+//     countryTemplateClone.querySelector("[data-population]");
+//   const languageElement = countryTemplateClone.querySelector("[data-language]");
+//   const currencyElement = countryTemplateClone.querySelector("[data-currency]");
+
+//   if (isNeighboringCountry) countryElement.classList.add("neighbour");
+
+//   imageElement.src = country.flag;
+//   nameElement.textContent = country.name;
+//   regionElement.textContent = country.region;
+//   populationElement.textContent = (country.population / 1000000).toFixed(1);
+//   languageElement.textContent = country.languages[0].name;
+//   currencyElement.textContent = country.currencies[0].name;
+
+//   countriesContainer.append(countryTemplateClone);
+//   countriesContainer.style.opacity = 1;
+// };
+
+// const getCountryAndNeighboringCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+//   request.addEventListener("load", function () {
+//     const [country] = JSON.parse(request.response);
+//     renderCountry(country);
+
+//     const countryNeighborCode = country.borders?.[0];
+
+//     if (!countryNeighborCode) return;
+
+//     const countryNeighborRequest = new XMLHttpRequest();
+//     countryNeighborRequest.open(
+//       "GET",
+//       `https://restcountries.com/v2/alpha/${countryNeighborCode}`,
+//     );
+//     countryNeighborRequest.send();
+//     countryNeighborRequest.addEventListener("load", function () {
+//       const countryNeighbor = JSON.parse(countryNeighborRequest.response);
+//       renderCountry(countryNeighbor, { isNeighboringCountry: true });
+//     });
+//   });
+// };
+
+// getCountryAndNeighboringCountryData("Germany");
+
+// // Callback hell example of a callback based async task chain that's not AJAX
+// setTimeout(() => {
+//   console.log("1 second has passed");
+//   setTimeout(() => {
+//     console.log("2 seconds have passed");
+//     setTimeout(() => {
+//       console.log("3 seconds have passed");
+//       setTimeout(() => {
+//         console.log("4 seconds have passed");
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+// console.log("This gets logged before all timeouts");
+// #endregion
+//////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////
+// #region 264. Consuming Promises
+///////////////////////////////////
 const renderCountry = function (
   country,
   { isNeighboringCountry = false } = {},
@@ -79,46 +156,12 @@ const renderCountry = function (
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighboringCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.com/v2/name/${country}`);
-  request.send();
-  request.addEventListener("load", function () {
-    const [country] = JSON.parse(request.response);
-    renderCountry(country);
-
-    const countryNeighborCode = country.borders?.[0];
-
-    if (!countryNeighborCode) return;
-
-    const countryNeighborRequest = new XMLHttpRequest();
-    countryNeighborRequest.open(
-      "GET",
-      `https://restcountries.com/v2/alpha/${countryNeighborCode}`,
-    );
-    countryNeighborRequest.send();
-    countryNeighborRequest.addEventListener("load", function () {
-      const countryNeighbor = JSON.parse(countryNeighborRequest.response);
-      renderCountry(countryNeighbor, { isNeighboringCountry: true });
-    });
-  });
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
 };
 
-getCountryAndNeighboringCountryData("Germany");
-
-// Callback hell example of a callback based async task chain that's not AJAX
-setTimeout(() => {
-  console.log("1 second has passed");
-  setTimeout(() => {
-    console.log("2 seconds have passed");
-    setTimeout(() => {
-      console.log("3 seconds have passed");
-      setTimeout(() => {
-        console.log("4 seconds have passed");
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
-console.log("This gets logged before all timeouts");
+btn.addEventListener("click", () => getCountryData("Poland"));
 // #endregion
 //////////////////////////////////////////////////////////////////////

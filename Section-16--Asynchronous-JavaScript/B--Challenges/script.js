a49f058aa6f1b65492ac029cb28d640d958dae7a
Maximiliano Ely Mazzola <max1_max@hotmail.com>
2026-04-05 12:05:36 +0200

    add completed section 16 coding challenge #1

--- Section-16--Asynchronous-JavaScript/B--Challenges/script.js
+++ Section-16--Asynchronous-JavaScript/B--Challenges/script.js
@@ -30,7 +30,6 @@ const renderCountry = function (
   currencyElement.textContent = country.currencies[0].name;
 
   countriesContainer.append(countryTemplateClone);
-  countriesContainer.style.opacity = 1;
 };
 
 //////////////////////////////////////////////////////////////////////
@@ -38,22 +37,73 @@ const renderCountry = function (
 //////////////////////////////////////////////////////////////////////
 // In this challenge you will build a function 'whereAmI' which renders a country only based on GPS coordinates. For that, you will use a second API to geocode coordinates. So in this challenge, you’ll use an API on your own for the first time
 
+const whereAmI = function (lat, lng) {
+  fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`,
+  )
+    .then(response => {
+      if (response.status == 403) {
+        const errorMessage = `${response.status} Exceeded the max amount of 3 requests per second`;
+        countriesContainer.textContent = errorMessage;
+        throw new Error(errorMessage);
+      }
+
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}`;
+        countriesContainer.textContent = errorMessage;
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      return response.json();
+    })
+    .then(data => {
+      if (!data.countryCode) {
+        const errorMessage = "You're not in a country or city";
+        countriesContainer.textContent = errorMessage;
+        throw new Error("You're not in a country or city");
+      }
+      console.log(`You are in ${data.city}, ${data.countryName}`);
+      return fetch(`https://restcountries.com/v2/name/${data.countryName}`);
+    })
+    .then(response => {
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}`;
+        countriesContainer.textContent = errorMessage;
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      return response.json();
+    })
+    .then(data => {
+      const [country] = data;
+      console.log(country);
+      renderCountry(country);
+    })
+    .catch(error => console.error(error))
+    .finally(() => (countriesContainer.style.opacity = 1));
+};
+
+// btn.addEventListener("click", () => whereAmI(-80.0, 0.0));
+// btn.addEventListener("click", () => whereAmI(52.508, 13.381));
+// btn.addEventListener("click", () => whereAmI(19.037, 72.873));
+btn.addEventListener("click", () => whereAmI(-33.933, 18.474));
+
 // Your tasks:
 // PART 1
-// 1. [ ] Create a function 'whereAmI' which takes as inputs a latitude value ('lat') and a longitude value ('lng') (these are GPS coordinates, examples are in test data below).
+// 1. [✔] Create a function 'whereAmI' which takes as inputs a latitude value ('lat') and a longitude value ('lng') (these are GPS coordinates, examples are in test data below).
 
-// 2. [ ] Do “reverse geocoding” of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://api.bigdatacloud.net/data/reverse-geocode-client. The AJAX call will be done to a URL with this format: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}. Use the fetch API and promises to get the data. Do not use the 'getJSON' function we created, that is cheating.
+// 2. [✔] Do “reverse geocoding” of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://api.bigdatacloud.net/data/reverse-geocode-client. The AJAX call will be done to a URL with this format: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}. Use the fetch API and promises to get the data. Do not use the 'getJSON' function we created, that is cheating.
 
-// 3. [ ] Once you have the data, take a look at it in the console to see all the attributes that you received about the provided location. Then, using this data, log a message like this to the console: “You are in Berlin, Germany”
+// 3. [✔] Once you have the data, take a look at it in the console to see all the attributes that you received about the provided location. Then, using this data, log a message like this to the console: “You are in Berlin, Germany”
 
-// 4. [ ] Chain a .catch method to the end of the promise chain and log errors to the console
+// 4. [✔] Chain a .catch method to the end of the promise chain and log errors to the console
 
-// 5. [ ] This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does not reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message
+// 5. [✔] This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does not reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message
 
 // PART 2
-// 6. [ ] Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+// 6. [✔] Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
 
-// 7. [ ] Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+// 7. [✔] Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
 
 // Test data:
 // - Coordinates 1: 52.508, 13.381 (Latitude, Longitude)
